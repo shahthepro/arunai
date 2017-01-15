125adbca01f4cc8b00d3ddc8df5837cb4e9321de
Shahul Hameed <shahthepro@gmail.com>
2017-01-15 18:36:50 +0530

    Update .gitignore

--- config/assets/production.js
+++ config/assets/production.js
@@ -12,6 +12,7 @@ module.exports = {
         'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
         'public/lib/AdminLTE/dist/css/AdminLTE.min.css',
         'public/lib/components-font-awesome/css/font-awesome.min.css',
+        'public/lib/angular-bootstrap-toggle/dist/angular-bootstrap-toggle.min.css',
         // endbower
       ],
       js: [
@@ -24,11 +25,17 @@ module.exports = {
         'public/lib/angular-resource/angular-resource.min.js',
         'public/lib/angular-ui-notification/dist/angular-ui-notification.min.js',
         'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+        'public/lib/jquery/dist/core.js',
+        'public/lib/jquery/dist/jquery.min.js',
+        'public/lib/jquery/dist/jquery.min.js',
+        'public/lib/jquery/dist/jquery.slim.min.js',
+        'public/lib/jquery/dist/jquery.slim.min.js',
         'public/lib/ng-file-upload/ng-file-upload.min.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
         'public/lib/AdminLTE/dist/js/app.min.js',
         'public/lib/angular-file-upload/dist/angular-file-upload.min.js',
         'public/lib/Papa-Parse/papaparse.min.js',
+        'public/lib/angular-bootstrap-toggle/dist/angular-bootstrap-toggle.min.js',
         // endbower
       ]
     },
