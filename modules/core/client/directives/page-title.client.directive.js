08c70c0c6e3779dc3253ddcf59189e1df2e68167
Shahul Hameed <shahthepro@gmail.com>
2017-03-25 21:28:44 +0530

    Update Homepage styles and add featured header

--- modules/core/client/directives/page-title.client.directive.js
+++ modules/core/client/directives/page-title.client.directive.js
@@ -2,9 +2,11 @@
   'use strict';
 
   angular.module('core')
-    .directive('pageTitle', pageTitle);
+    .directive('pageTitle', pageTitle)
+    .directive('pageHeader', pageHeader);
 
   pageTitle.$inject = ['$rootScope', '$interpolate', '$state'];
+  pageHeader.$inject = ['$rootScope', '$interpolate', '$state'];
 
   function pageTitle($rootScope, $interpolate, $state) {
     var directive = {
@@ -18,11 +20,35 @@
       $rootScope.$on('$stateChangeSuccess', listener);
 
       function listener(event, toState) {
-        var applicationCoreTitle = 'Arunai Portal',
+        var applicationCoreTitle = 'Arunai Engineering College',
           separeteBy = ' - ';
         if (toState.data && toState.data.pageTitle) {
           var stateTitle = $interpolate(toState.data.pageTitle)($state.$current.locals.globals);
           element.html(applicationCoreTitle + separeteBy + stateTitle);
+        } else {
+          element.html(applicationCoreTitle);
+        }
+      }
+    }
+  }
+
+  function pageHeader($rootScope, $interpolate, $state) {
+    var directive = {
+      restrict: 'A',
+      link: link
+    };
+
+    return directive;
+
+    function link(scope, element) {
+      $rootScope.$on('$stateChangeSuccess', listener);
+
+      function listener(event, toState) {
+        var applicationCoreTitle = 'Arunai Engineering College',
+          separeteBy = ' - ';
+        if (toState.data && toState.data.pageTitle) {
+          var stateTitle = $interpolate(toState.data.pageTitle)($state.$current.locals.globals);
+          element.html(stateTitle);
         } else {
           element.html(applicationCoreTitle);
         }
